9aab4744f27409e035f19fa6442f7bfeabb8780f
Critina Repollo <repollocristina0@gmail.com>
2024-12-25 18:45:57 +0800

    first commit

--- AppA/user_messages/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Message
-
-# Register the Message model so it appears in the Django admin
-admin.site.register(Message)
